8cf26fcdce74a308aa6ca90346724da3432e9d48
Alistair Robinson <alistair@commissioncrowd.com>
2023-11-21 13:31:07 +0300

    Refactor

--- app.py
+++ app.py
@@ -23,12 +23,10 @@ def verify_signature(payload_body, secret_token, signature_header):
         secret_token: GitHub app webhook token (WEBHOOK_SECRET)
         signature_header: header received from GitHub (x-hub-signature-256)
     """
-    if not signature_header:
-        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
-    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
-    expected_signature = "sha256=" + hash_object.hexdigest()
-    if not hmac.compare_digest(expected_signature, signature_header):
-        raise HTTPException(status_code=403, detail="Request signatures didn't match!")
+    if signature_header:
+        hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+        expected_signature = "sha256=" + hash_object.hexdigest()
+        return hmac.compare_digest(expected_signature, signature_header)
 
 #def verify_signature(req):
 #     received_sign = req.headers.get('X-Hub-Signature-256').split('sha256=')[-1].strip()
